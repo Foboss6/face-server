72e6437f0694405610433dfdb8ea7e50c95ef67b
Foboss <foboss6@gmail.com>
2021-09-09 11:14:28 +0300

    preparetion to deploying

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ import handleSignin from './controllers/signin.js';
 import handleProfile from './controllers/profile.js';
 import handleImage from './controllers/image.js';
 
+const DATABASE_URL = process.env.DATABASE_URL;
+// To set DATABASE_URL on Windows write next command in CMD:
+// [Environment]::SetEnvironmentVariable("DATABASE_URL", "1235")
+// where 1235 is the numb of your port
+// To see a list all environment variables make
+// Get-ChildItem Env:    or     Get-ChildItem Env:DATABASE_URL
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -38,4 +45,4 @@ app.put('/image', (req, res) => { handleImage(req, res, db) });
 // maybe we'll use it in future
 app.put('/profile/:id',  handleProfile(db));
 
-app.listen(3001, ()=>{console.log('server is runnig')});
+app.listen(DATABASE_URL, ()=>{console.log(`server is listening on port: ${DATABASE_URL}`)});
